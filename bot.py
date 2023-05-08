dfb9965bde3def7743ae8dfe061f6f6dbc88cf0b
Bob <bboland15@gmail.com>
2023-05-08 16:23:31 -0500

    learning about Intents, discord.ext is next

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import os
 
 import discord
+from discord.ext import commands
+
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 intents = discord.Intents.default()
 intents.message_content = True
