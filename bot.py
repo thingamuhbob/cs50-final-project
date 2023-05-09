6b9f32f21d27425c44d75e0c89a658a2f7bc7659
Bob <bboland15@gmail.com>
2023-05-09 11:21:32 -0500

    starting a timeout words list, adding a WoT.db for stored lists

--- bot.py
+++ bot.py
@@ -11,18 +11,13 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
-    
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
         
-client.run(TOKEN)
+
+        
+bot.run(TOKEN)
