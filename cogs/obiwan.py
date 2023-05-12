fd9b2275dada16d6f9452fd92e4c4e420915ac3a
Bob <bboland15@gmail.com>
2023-05-11 19:36:02 -0500

    hello_there.gif reply

--- cogs/obiwan.py
+++ cogs/obiwan.py
@@ -1,3 +1,4 @@
+import discord
 import random
 import sqlite3
 from discord.ext import commands
@@ -22,6 +23,17 @@ class Obiwan(commands.Cog):
 
         quote = random.choice(self.quotes)
         await ctx.send(f'"{quote}"')
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        # Don't listen to yourself, bot
+        if message.author == self.bot.user:
+            return
+
+        if message.content.lower() == "hello":
+            with open("assets/hello_there.gif", "rb") as f:
+                picture = discord.File(f, filename="hello_there.gif")
+            await message.reply(file=picture)
 
 
 async def setup(bot):
